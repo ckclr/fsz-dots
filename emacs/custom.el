ed55da34dbfcac0f8d1608a388f51e06707abb8d
jianxiah <cookcooller@163.com>
2022-12-11 15:37:53 +0800

    add emacs

---
+++ emacs/custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(org-fragtog which-key vertico use-package rg restart-emacs projectile orderless marginalia expand-region eglot company blacken benchmark-init ace-window)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0))))
+ '(fixed-pitch ((t (:family "Sarasa Term Slab SC" :height 120))))
+ '(variable-pitch ((t (:family "Sarasa Term Slab SC" :height 120 :weight thin)))))
